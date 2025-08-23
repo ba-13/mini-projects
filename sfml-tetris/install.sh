03c2fa26993bb08bcfe65ba40835e1a3a5faba16
Anshuman Barnwal <b.a.13.official@gmail.com>
2025-08-23 11:30:21 +0200

    add sfml-install shell file

---
+++ sfml-tetris/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt update && sudo apt install \
+     libxrandr-dev \
+     libxcursor-dev \
+     libxi-dev \
+     libudev-dev \
+     libflac-dev \
+     libvorbis-dev \
+     libgl1-mesa-dev \
+     libegl1-mesa-dev \
+     libdrm-dev \
+     libgbm-dev
+
+sudo apt-get install libglew-dev
